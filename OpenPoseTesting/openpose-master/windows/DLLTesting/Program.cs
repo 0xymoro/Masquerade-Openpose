40b229da0449e1c9be9a3badb1ba84a6300d98cb
Jerry Meng <zmeng90@stanford.edu>
2017-12-03 20:51:13 -0800

    Experiment with dll and dependencies in DLLTesting project, fix dll not found

---
+++ OpenPoseTesting/openpose-master/windows/DLLTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DLLTesting
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\RHVR3.RHVR3\Desktop\Projects\Jerry\OpenPosePlayground\OpenPoseTesting\openpose-master\windows\DLLTesting\bin\Release\extract_from_image.dll", CallingConvention = CallingConvention.Cdecl)]
+
+        public static extern int openPoseTutorialPose1();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(openPoseTutorialPose1());
+            Console.WriteLine("Click any key to continue...");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
